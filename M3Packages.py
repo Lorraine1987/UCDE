44d99c84d045903f1ccb88a6d1c8a9e226308268
Lorraine1987 <obyrneloraine@hotmail.com>
2021-03-12 08:22:55 +0000

    completion of exercise 3

---
+++ M3Packages.py
@@ -0,0 +1,26 @@
+# Import the math package
+import math
+
+# Definition of radius
+r = 0.43
+
+# Calculate C
+C = 2*math.pi*r
+
+# Calculate A
+A = math.pi*r*r
+
+# Build printout
+print("Circumference: " + str(C))
+print("Area: " + str(A))
+
+# Definition of radius
+r = 192500
+
+from math import radians
+
+# Travel distance of Moon over 12 degrees. Store in dist.
+dist = r*radians(12)
+
+# Print out dist
+print(dist)
