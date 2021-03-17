34b107de5b19b99cebb3436e335e51c1a3f2bc5d
Lorraine1987 <obyrneloraine@hotmail.com>
2021-03-17 11:07:46 +0000

    completion of exercise 4

---
+++ M4.py
@@ -0,0 +1,183 @@
+# Import the numpy package as np
+import numpy as np
+
+# Import the pandas package as pd
+import pandas as pd
+
+# Create list baseball
+baseball = [180, 215, 210, 210, 188, 176, 209, 200]
+
+# Create a numpy array from baseball: np_baseball
+np_baseball = np.array(baseball)
+
+# Print out type of np_baseball
+print(type(np_baseball))
+
+height_in = pd.read_csv(r'C:\UCD\height_in.csv')
+
+# Create a numpy array from height_in: np_height_in
+np_height_in = np.array(height_in)
+
+# Print out np_height_in
+print(np_height_in)
+
+# Convert np_height_in to m: np_height_m
+np_height_m = np_height_in * 0.0254
+
+# Print np_height_m
+print(np_height_m)
+
+weight_lb = pd.read_csv(r'C:\UCD\weight_lb.csv')
+
+# Create array from weight_lb with metric units: np_weight_kg
+np_weight_kg = np.array(weight_lb) * 0.453592
+
+print(np_weight_kg)
+
+# Calculate the BMI: bmi
+# bmi = np_weight_kg / np_height_m ** 2
+
+# Print out bmi
+# print(bmi)
+
+# Calculate the BMI: bmi
+# np_height_m = np.array(height_in) * 0.0254
+# np_weight_kg = np.array(weight_lb) * 0.453592
+# bmi = np_weight_kg / np_height_m ** 2
+
+# Create the light array
+# light = bmi < 21
+
+# Print out light
+# print(light)
+
+# Print out BMIs of all baseball players whose BMI is below 21
+# print(bmi[bmi < 21])
+
+# height and weight are available as a regular lists
+
+# Import numpy
+# import numpy as np
+
+# Store weight and height lists as numpy arrays
+np_weight_lb = np.array(weight_lb)
+np_height_in = np.array(height_in)
+
+# Print out the weight at index 50
+print(np_weight_lb[50])
+
+# Print out sub-array of np_height_in: index 100 up to and including index 110
+print(np_height_in[100:111])
+
+# Create baseball, a list of lists
+baseball = [[180, 78.4],
+            [215, 102.7],
+            [210, 98.5],
+            [188, 75.2]]
+
+# Import numpy
+# import numpy as np
+
+# Create a 2D numpy array from baseball: np_baseball
+np_baseball = np.array(baseball)
+
+# Print out the type of np_baseball
+print(type(np_baseball))
+
+# Print out the shape of np_baseball
+print(np_baseball.shape)
+
+# baseball is available as a regular list of lists
+
+# Create a 2D numpy array from baseball: np_baseball
+# np_baseball = np.array(baseball)
+
+# Print out the shape of np_baseball
+# print(np_baseball.shape)
+# (1015, 2)
+
+# baseball is available as a regular list of lists
+
+# Create np_baseball (2 cols)
+# np_baseball = np.array(baseball)
+
+# Print out the 50th row of np_baseball
+# print(np_baseball[49,:])
+# (70 195)
+
+# Select the entire second column of np_baseball: np_weight_lb
+# np_weight_lb = np_baseball[:, 1]
+
+# Print out height of 124th player
+# print(np_baseball[123, 0])
+# 75
+
+# baseball is available as a regular list of lists
+# updated is available as 2D numpy array
+
+# Create np_baseball (3 cols)
+# np_baseball = np.array(baseball)
+
+# Print out addition of np_baseball and updated
+# print(np_baseball + updated)
+
+# Create numpy array: conversion
+conversion = (0.0254, 0.453592, 1)
+conversion = np.array(conversion)
+
+# Print out product of np_baseball and conversion
+# print(np_baseball * conversion)
+
+# np_baseball is available
+
+# Create np_height_in from np_baseball
+# np_height_in = np_baseball[:, 0]
+
+# Print out the mean of np_height_in
+# print(np.mean(np_height_in))
+# 1586.46
+
+# Print out the median of np_height_in
+# print(np.median(np_height_in))
+# 74
+
+# np_baseball is available
+
+# Print mean height (first column)
+# avg = np.mean(np_baseball[:,0])
+# print("Average: " + str(avg))
+# 73.69
+
+# Print median height. Replace 'None'
+# med = np.median(np_baseball[:, 0])
+# print("Median: " + str(med))
+# 74
+
+# Print out the standard deviation on height. Replace 'None'
+# stddev = np.std(np_baseball[:, 0])
+# print("Standard Deviation: " + str(stddev))
+# 2.31
+
+# Print out correlation between first and second column. Replace 'None'
+# corr = np.corrcoef(np_baseball[:, 0], np_baseball[:, 1])
+# print("Correlation: " + str(corr))
+# Correlation: [[1.         0.53153932]
+#      [0.53153932 1.        ]]
+
+# heights and positions are available as lists
+
+# Convert positions and heights to numpy arrays: np_positions, np_heights
+# np_positions = np.array(positions)
+# np_heights = np.array(heights)
+
+# Heights of the goalkeepers: gk_heights
+# gk_heights = np_heights[np_positions == "GK"]
+
+# Heights of the other players: other_heights
+# other_heights = np_heights[np_positions != "GK"]
+
+# Print out the median height of goalkeepers. Replace 'None'
+# print("Median height of goalkeepers: " + str(np.median(gk_heights)))
+
+# Print out the median height of other players. Replace 'None'
+# print("Median height of other players: " + str(np.median(other_heights)))
