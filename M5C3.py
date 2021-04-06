a3b67310f9cf52aedca5bf9670abdf7e73f66f57
Lorraine1987 <obyrneloraine@hotmail.com>
2021-04-06 08:49:38 +0100

    Completion of Module 5 and start of Module 6

--- M5C3.py
+++ M5C3.py
@@ -93,7 +93,9 @@ print(temperatures.iloc[:, 2:4])
 print(temperatures.iloc[0:5, 2:4])
 
 # Add a year column to temperatures
-temperatures['year'] = temperatures['date'].dt.year
+temperatures['date'] = pd.to_datetime(temperatures['date'])
+
+temperatures["year"] = temperatures["date"].dt.year
 
 # Pivot avg_temp_c by country and city vs year
 temp_by_country_city_vs_year = temperatures.pivot_table(values="avg_temp_c", index=["country", "city"], columns="year")
@@ -109,6 +111,18 @@ print(temp_by_country_city_vs_year.loc[("Egypt", "Cairo"):("India", "Delhi")])
 
 # Subset in both directions at once
 print(temp_by_country_city_vs_year.loc[("Egypt", "Cairo"):("India", "Delhi"), "2005":"2010"])
+
+# Get the worldwide mean temp by year
+mean_temp_by_year = temp_by_country_city_vs_year.mean()
+
+# Filter for the year that had the highest mean temp
+print(mean_temp_by_year[mean_temp_by_year==mean_temp_by_year.max()])
+
+# Get the mean temp by city
+mean_temp_by_city = temp_by_country_city_vs_year.mean(axis="columns")
+
+# Filter for the city that had the lowest mean temp
+print(mean_temp_by_city[mean_temp_by_city==mean_temp_by_city.min()])
 
 
 
