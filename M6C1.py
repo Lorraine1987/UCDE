1b3505a321eedc08509f6466a66d229bfea33498
Lorraine1987 <obyrneloraine@hotmail.com>
2021-04-07 10:03:30 +0100

    Continuation of Module 6

--- M6C1.py
+++ M6C1.py
@@ -52,3 +52,40 @@ sorted_df = counted_df.sort_values("account", ascending=False)
 # Use .head() method to print the first few rows of sorted_df
 print(sorted_df.head())
 
+# Merge the ridership and cal tables
+ridership_cal = ridership.merge(cal, on=['year', 'month', 'day'])
+
+# Merge the ridership, cal, and stations tables
+ridership_cal_stations = ridership.merge(cal, on=['year', 'month', 'day']) \
+            				.merge(stations, on='station_id')
+
+# Create a filter to filter ridership_cal_stations
+filter_criteria = ((ridership_cal_stations['month'] == 7)
+                   & (ridership_cal_stations['day_type'] == 'Weekday')
+                   & (ridership_cal_stations['station_name'] == 'Wilson'))
+
+# Use .loc and the filter to select for rides
+print(ridership_cal_stations.loc[filter_criteria, 'rides'].sum())
+
+# Merge licenses and zip_demo, on zip; and merge the wards on ward
+licenses_zip_ward = licenses.merge(zip_demo, on='zip') \
+            			.merge(wards,  on='ward')
+
+# Print the results by alderman and show median income
+print(licenses_zip_ward.groupby('alderman').agg({'income':'median'}))
+
+# Merge land_use and census and merge result with licenses including suffixes
+land_cen_lic = land_use.merge(census, on='ward')\
+                    .merge(licenses, on='ward',suffixes=('_cen', '_lic'))
+
+# Group by ward, pop_2010, and vacant, then count the # of accounts
+                                   as_index=False).agg({'account':'count'})
+
+# Sort pop_vac_lic and print the results
+sorted_pop_vac_lic = pop_vac_lic.sort_values(['vacant', 'account',                   'pop_2010'], ascending=[False, True, True])
+
+# Print the top few rows of sorted_pop_vac_lic
+print(sorted_pop_vac_lic.head())
+
+
+
